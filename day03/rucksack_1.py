b1a2da86616e47f0cc04c0bf9b9664ae2db6cb86
Lucas Peres <lucasperes95@gmail.com>
2022-12-04 00:21:51 -0300

    refactored: no for loop, just generator

--- day03/rucksack_1.py
+++ day03/rucksack_1.py
@@ -3,13 +3,10 @@
 import fileinput
 from string import ascii_letters
 
-total = 0
+def priotiry(rucksack):
+    mid = len(rucksack) // 2
+    first, second = set(rucksack[:mid]), set(rucksack[mid:])
+    return sum(ascii_letters.index(char) + 1 for char in first & second)
 
 with fileinput.input() as fin:
-    for line in fin:
-        line = line.strip()
-        mid = len(line) // 2
-        first, second = set(line[:mid]), set(line[mid:])
-        total += sum(ascii_letters.index(char) + 1 for char in first & second)
-
-print(total)
+    print(sum(priotiry(line.strip()) for line in fin))
