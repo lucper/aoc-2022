b201ab3810d9f2eca7a142be7838a60a1dcceac8
Lucas Peres <lucasperes95@gmail.com>
2022-12-06 10:07:31 -0300

    replacing 'for' with 'while'

--- day06/tuning_trouble.py
+++ day06/tuning_trouble.py
@@ -8,7 +8,8 @@ MSG_LEN = 14
 with fileinput.input() as fin:
     signal = fin.readline().strip()
 
-for i, _ in enumerate(signal[:-(MSG_LEN - 1)]):
-    if len(set(signal[i:i + MSG_LEN])) == MSG_LEN:
-        print(i + MSG_LEN)
-        break
+i = 0
+while len(set(signal[i:i + MSG_LEN])) != MSG_LEN:
+    i += 1
+
+print(i + MSG_LEN)
