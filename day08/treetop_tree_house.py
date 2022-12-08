178192a01d79ceda87f6e5c020e90e1b0b278b5d
Lucas Peres <lucasperes95@gmail.com>
2022-12-08 16:59:45 -0300

    renamed counting function

--- day08/treetop_tree_house.py
+++ day08/treetop_tree_house.py
@@ -14,7 +14,7 @@ def top_visible(i, j, grid):
 def bottom_visible(i, j, grid):
     return grid[i][j] > max(grid[k][j] for k in range(i + 1, len(grid)))
 
-def count_visible_trees(grid):
+def count_interior_trees(grid):
     return sum(1 for i in range(1, len(grid) - 1) 
                  for j in range(1, len(grid[0]) - 1)
                  if left_visible(i, j, grid) or
@@ -26,4 +26,4 @@ with fileinput.input() as fin:
     trees = [[int(c) for c in list(line.strip())] for line in fin]
 
 outer_trees = (2 * len(trees)) + (2 * len(trees[0])) - 4
-print(count_visible_trees(trees) + outer_trees)
+print(count_interior_trees(trees) + outer_trees)
