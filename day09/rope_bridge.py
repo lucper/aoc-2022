9f24e4b40dd2fa3a723a9f047ebf65f77d9bb2f8
Lucas Peres <lucasperes95@gmail.com>
2022-12-09 04:43:28 -0300

    notes for later

--- day09/rope_bridge.py
+++ day09/rope_bridge.py
@@ -7,6 +7,19 @@ def touching(pos1, pos2):
     c, d = pos2
     return abs(a - c) <= 1 and abs(b - d) <= 1
 
+## TODO
+## if not in_same_axis(tail, head), then
+## tail := (x+1,y+1) if a < c and b < d
+## or
+## tail := (x-1,y-1) if a > c and b > d
+##
+## if in_same_axis(tail, head), then
+## ...
+def in_same_axis(pos1, pos2):
+    a, b = pos1
+    c, d = pos2
+    return a == c or b == d
+
 def walk_right(pos, k):
     i, j = pos
     return [(x, j) for x in range(i + 1, i + k + 1, 1)]
@@ -24,21 +37,21 @@ def walk_down(pos, k):
     return [(i, y) for y in range(j - 1, j - k - 1, -1)]
 
 with fileinput.input() as fin:
-    curr_pos = (0, 0)
+    head = (0, 0)
     for line in fin:
         direction, steps = line.strip().split()
         steps = int(steps)
         if direction == 'L':
-            *track, curr_pos = walk_left(curr_pos, steps)
-            print(track + [curr_pos])
+            *track, head = walk_left(head, steps)
+            print(track + [head])
         elif direction == 'R':
-            *track, curr_pos = walk_right(curr_pos, steps)
-            print(track + [curr_pos])
+            *track, head = walk_right(head, steps)
+            print(track + [head])
         elif direction == 'U':
-            *track, curr_pos = walk_up(curr_pos, steps)
-            print(track + [curr_pos])
+            *track, head = walk_up(head, steps)
+            print(track + [head])
         elif direction == 'D':
-            *track, curr_pos = walk_down(curr_pos, steps)
-            print(track + [curr_pos])
+            *track, head = walk_down(head, steps)
+            print(track + [head])
         else:
             raise ValueError('invalid command')
