3c72b81f1eb66140c019138e2e98601e7d30b608
Lucas Peres <lucasperes95@gmail.com>
2022-12-10 02:57:44 -0300

    final final soln (MUCH better :D)

--- day09/rope_bridge.py
+++ day09/rope_bridge.py
@@ -2,6 +2,7 @@
 
 import fileinput
 from math import copysign
+from operator import add
 
 def sign(n):
     return int(copysign(1, n)) if n != 0 else 0
@@ -16,26 +17,18 @@ def catch_up(src, dest):
 number_of_knots = 10
 rope = [(0,0) for _ in range(number_of_knots)]
 visited = {rope[-1]}
+vector = {'L': (-1, 0),
+          'R': (1, 0),
+          'U': (0, 1),
+          'D': (0, -1)}
 
 with fileinput.input() as fin:
     for line in fin:
         direction, steps = line.strip().split()
         for _ in range(int(steps)):
-            x, y = rope[0]
-            if direction == 'L':
-                rope[0] = (x - 1, y)
-            elif direction == 'R':
-                rope[0] = (x + 1, y)
-            elif direction == 'U':
-                rope[0] = (x, y + 1)
-            elif direction == 'D':
-                rope[0] = (x, y - 1)
-            else:
-                raise ValueError('invalid command')
-
+            rope[0] = tuple(map(add, rope[0], vector[direction]))
             for head, (tail_idx, tail) in zip(rope, enumerate(rope[1:], 1)):
                 rope[tail_idx] = catch_up(tail, head)
-
             visited.add(rope[tail_idx])
 
 print(len(visited))
