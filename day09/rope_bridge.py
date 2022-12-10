d5753ba06cee6dce1ea606a8aef6bfca4158c989
Lucas Peres <lucasperes95@gmail.com>
2022-12-10 02:38:44 -0300

    final soln

--- day09/rope_bridge.py
+++ day09/rope_bridge.py
@@ -12,6 +12,7 @@ def catch_up(src, dest):
         a, b = a + sign(c - a), b + sign(d - b)
     return a, b
 
+## change number_of_knots to 2 for Part 1
 number_of_knots = 10
 rope = [(0,0) for _ in range(number_of_knots)]
 visited = {rope[-1]}
@@ -19,44 +20,22 @@ visited = {rope[-1]}
 with fileinput.input() as fin:
     for line in fin:
         direction, steps = line.strip().split()
-        steps = int(steps)
-        if direction == 'L':
-            for _ in range(steps):
-                x, y = rope[0]
+        for _ in range(int(steps)):
+            x, y = rope[0]
+            if direction == 'L':
                 rope[0] = (x - 1, y)
-
-                for head, (tail_idx, tail) in zip(rope, enumerate(rope[1:], 1)):
-                    rope[tail_idx] = catch_up(tail, head)
-
-                visited.add(rope[tail_idx])
-        elif direction == 'R':
-            for _ in range(steps):
-                x, y = rope[0]
+            elif direction == 'R':
                 rope[0] = (x + 1, y)
-
-                for head, (tail_idx, tail) in zip(rope, enumerate(rope[1:], 1)):
-                    rope[tail_idx] = catch_up(tail, head)
-
-                visited.add(rope[tail_idx])
-        elif direction == 'U':
-            for _ in range(steps):
-                x, y = rope[0]
+            elif direction == 'U':
                 rope[0] = (x, y + 1)
-
-                for head, (tail_idx, tail) in zip(rope, enumerate(rope[1:], 1)):
-                    rope[tail_idx] = catch_up(tail, head)
-
-                visited.add(rope[tail_idx])
-        elif direction == 'D':
-            for _ in range(steps):
-                x, y = rope[0]
+            elif direction == 'D':
                 rope[0] = (x, y - 1)
+            else:
+                raise ValueError('invalid command')
 
-                for head, (tail_idx, tail) in zip(rope, enumerate(rope[1:], 1)):
-                    rope[tail_idx] = catch_up(tail, head)
+            for head, (tail_idx, tail) in zip(rope, enumerate(rope[1:], 1)):
+                rope[tail_idx] = catch_up(tail, head)
 
-                visited.add(rope[tail_idx])
-        else:
-            raise ValueError('invalid command')
+            visited.add(rope[tail_idx])
 
 print(len(visited))
