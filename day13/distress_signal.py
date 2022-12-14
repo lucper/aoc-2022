a5ee4c65188b9fa26b53b2bce0e926f7a3caac35
Lucas Peres <lucasperes95@gmail.com>
2022-12-14 00:54:06 -0300

    correct soln to part 1, but need to refactor

--- day13/distress_signal.py
+++ day13/distress_signal.py
@@ -4,24 +4,29 @@ import fileinput
 from ast import literal_eval
 
 def right_order(left, right):
-    if left and not right:
-        return False
-    if not left and right:
-        return True
-    if not left and not right:
-        return False
-    
-    if isinstance(left[0], int) and isinstance(right[0], int):
-        return left[0] < right[0] if left[0] != right[0] else right_order(left[1:], right[1:])
-    elif isinstance(left[0], int):
-        return right_order([[left[0]]] + left[1:], right)
-    elif isinstance(right[0], int):
-        return right_order(left, [[right[0]]] + right[1:])
+    ##  1 => true, in order
+    ## -1 => false, not in order
+    ##  0 => continue
+    if isinstance(left, int) and isinstance(right, int):
+        return 1 if left < right else -1 if left > right else 0
+    elif isinstance(left, int):
+        return right_order([left], right)
+    elif isinstance(right, int):
+        return right_order(left, [right])
     else:
-        return right_order(left[0], right[0]) or right_order(left[1:], right[1:])
+        if left and not right:
+            return -1
+        if not left and right:
+            return 1
+        if not left and not right:
+            return 0
+        if (r := right_order(left[0], right[0])) != 0:
+            return r
+        return right_order(left[1:], right[1:])
 
 with fileinput.input() as fin:
     pairs = [literal_eval(line.strip()) for line in fin if line.strip()]
-    count = sum(i for i, pair in enumerate(zip(pairs[::2], pairs[1::2]), 1) if right_order(*pair))
 
+## Part 1
+count = sum(i for i, pair in enumerate(zip(pairs[::2], pairs[1::2]), 1) if right_order(*pair) > 0)
 print(count)
